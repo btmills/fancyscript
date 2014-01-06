07a47c443b24012f36facf91cb13ecc1a8a94468
Brandon Mills <mills.brandont@gmail.com>
2014-01-06 01:12:57 -0500

    Fix bug with ignored values in array destructuring

--- plugins/arrayDestructuring.js
+++ plugins/arrayDestructuring.js
@@ -19,7 +19,7 @@ module.exports = function (compiler) {
 		var replacement = extend({}, node, { declarations: [] });
 		node.declarations.forEach(function (declaration) {
 			if (declaration.id.type !== 'ArrayPattern') {
-				return replacement.declarations.push(declaration);
+				replacement.declarations.push(declaration);
 			} else {
 				var temp = '$fsadtmp' + counter++;
 				replacement.declarations.push(b.variableDeclarator(
@@ -27,6 +27,7 @@ module.exports = function (compiler) {
 					declaration.init
 				));
 				declaration.id.elements.forEach(function (id, index) {
+					if (id === null) return false;
 					replacement.declarations.push(b.variableDeclarator(
 						id,
 						b.memberExpression(
