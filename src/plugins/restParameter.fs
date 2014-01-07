ebac22596a25dc9166c8979215e655889b650204
Brandon Mills <mills.brandont@gmail.com>
2014-01-07 01:39:30 -0500

    Port rest parameter plugin

---
+++ src/plugins/restParameter.fs
@@ -0,0 +1,39 @@
+var extend = require('extend');
+var b = require('ast-types').builders;
+
+module.exports = function (compiler) {
+
+	// TOOD: What about ArrowFunctionExpression?
+	compiler.on(['FunctionDeclaration', 'FunctionExpression'], {
+		'$.rest.type': 'Identifier'
+	}, function (node) {
+		var replacement = extend({}, node, { rest: null });
+		replacement.body.body.unshift(b.variableDeclaration(
+			'var',
+			[ b.variableDeclarator(
+				b.identifier(node.rest.name),
+				b.callExpression(
+					b.memberExpression(
+						b.memberExpression(
+							b.memberExpression(
+								b.identifier('Array'),
+								b.identifier('prototype'),
+								false
+							),
+							b.identifier('slice'),
+							false
+						),
+						b.identifier('call'),
+						false
+					),
+					[
+						b.identifier('arguments'),
+						b.literal(node.params.length)
+					]
+				)
+			) ]
+		));
+		return replacement;
+	});
+
+};
